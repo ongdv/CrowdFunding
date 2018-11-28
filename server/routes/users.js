1c008a0aa6f50f862f785fa2da458bccb4ecb2b6
Ubuntu <ubuntu@ip-172-31-25-162.ap-northeast-2.compute.internal>
2018-11-28 04:03:15 +0000

    create skeleton

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../config/dbConfig');
 
-/* GET users listing. */
+//작가명단뽑아오기
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  pool.getConnection(function(err, conn){
+    conn.release();
+    if(err){
+      res.send(201, {
+        result: 0
+      });
+    }
+    var sql = 'SELECT * FROM Client';
+    conn.query(sql, function(err, row){
+      if(err){
+        res.send(201, {
+          result: 0
+        });  
+      }
+      res.send(200, {
+        result: 1,
+        data: row
+      });
+    })
+  })
 });
 
 module.exports = router;
